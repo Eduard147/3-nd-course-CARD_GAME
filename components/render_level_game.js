415e9e5888b92de836db637597178003b525f670
Eduard <mr.dolboroaev@mail.ru>
2023-08-01 19:51:33 +0300

    Проверьте дз

--- components/render_level_game.js
+++
@@ -1,9 +0,0 @@
-export function renderLevelGame(level, appEl) {
-    const appHTML = `
-            <div class="top center">
-            <div class="box-game">       
-            <h2 class="box-game__title">${level.value} карточек</h2>
-            </div></div>
-            `;
-    appEl.innerHTML = appHTML;
-}
