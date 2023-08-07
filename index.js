b134c2340a3a7ba8abf96f2cd54b026b3d570ee5
Eduard <mr.dolboroaev@mail.ru>
2023-08-07 15:54:49 +0300

    Дз-4

--- index.js
+++ index.js
@@ -1,45 +1,5 @@
+import "./style/style.css";
 import { renderLevelGame } from "./components/render_level_game.js";
+import { renderGameDifficulty } from "./components/render.js";
 
-function renderGameDifficulty() {
-    let appEl = document.getElementById("app");
-
-    const appHTML = `
-    <div class="top center">
-    <form id="form-level" class="box-game" method="POST" action="#">
-        <h1 class="box-game__title">Выбери<br>сложность</h1>
-
-        <div class="box-difficulty-level">
-
-            <input class="box-difficulty-level__radio" type="radio" id="radio1" name="radios" value="6">
-            <label class="box-difficulty-level__label" for="radio1">1</label>
-
-            <input class="box-difficulty-level__radio" type="radio" id="radio2" name="radios" value="12">
-            <label class="box-difficulty-level__label" for="radio2">2</label>
-
-            <input class="box-difficulty-level__radio" type="radio" id="radio3" name="radios" value="18">
-            <label class="box-difficulty-level__label" for="radio3">3</label>
-
-        </div>
-        <button class="box-game__button" id="submit-button" type="submit">Старт</button>
-    </form>
-</div>`;
-
-    appEl.innerHTML = appHTML;
-
-    document.getElementById("form-level").addEventListener("submit", (e) => {
-        e.preventDefault();
-        console.log(e);
-
-        let levels = document.querySelectorAll(".box-difficulty-level__radio");
-
-        for (const level of levels) {
-            if (level.checked) {
-                console.log(level.value);
-
-                renderLevelGame(level, appEl);
-            }
-        }
-    });
-}
-
-renderGameDifficulty();
+renderGameDifficulty(renderLevelGame);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV;
+
 module.exports = {
     entry: "./index.js",
-    output: {
-        filename: "main.js",
+    mode: isProduction ? "production" : "development",
+    module: {
+        rules: [
+            { test: /\.scss$/, use: ["style-loader", "scss-loader"] },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+        ],
     },
+
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+        clean: true,
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: "static", to: "static" }], //сохраняет все статичные файлы
+        }),
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+        }),
+        new MiniCssExtractPlugin(),
+    ],
+    optimization: {
+        minimizer: ["...", new CssMinimizerPlugin()],
+    },
+    devtool: isProduction ? "hidden-source-map" : "source-map",
 };
