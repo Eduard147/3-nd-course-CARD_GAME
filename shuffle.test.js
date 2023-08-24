e2faadda0a4aa075d6212b769d82c5a97fcc75cf
Eduard <mr.dolboroaev@mail.ru>
2023-08-24 14:54:55 +0300

    Курсовая

--- shuffle.test.js
+++ shuffle.test.js
@@ -3,15 +3,14 @@ const { shuffle } = require("lodash");
 const { cardsSuitsArr } = require("./components/array_cards_suits");
 const { cardsSuitsArraySort } = require("./components/render_level_game");
 
-
 describe("lodash: shuffle", () => {
     test("should be defined", () => {
         expect(shuffle).toBeDefined();
     });
 
-    test('should not contain falsy values', () => {
-        expect(shuffle(cardsSuitsArraySort)).not.toContain(false)
-      })
+    test("should not contain falsy values", () => {
+        expect(shuffle(cardsSuitsArraySort)).not.toContain(false);
+    });
 });
 
 describe("Array: cardsSuitsArr", () => {
